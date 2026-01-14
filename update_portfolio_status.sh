4748c776ee9c2f7902b93e12a6a92863e872a8f2
caretak3r <50377477+caretak3r@users.noreply.github.com>
2026-01-14 09:13:49 -0500

    Remove obsolete update_portfolio_status.sh - GitHub Actions handles this now

--- update_portfolio_status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Status updates are now handled via fix_portfolio_json.js
-# This script was corrupting the JSON syntax
-echo 'Use: cd ~/clawd/portfolio-analysis && node ~/clawd/portfolio-analysis/fix_portfolio_json.js'
-exit 1
-
